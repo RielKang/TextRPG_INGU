ddd149dda48927bfaeb4dc3ad631f424919dea25
KangIngu <rielkang99@gmail.com>
2025-07-11 19:04:48 +0900

    fd

--- TextRPG_INGU/ShopItemDatabase.cs
+++ TextRPG_INGU/ShopItemDatabase.cs
@@ -22,13 +22,13 @@ namespace TextRPG_INGU
                 new ShopItem("부러진 붉은 깃창", "공격력 +1", "부러진 붉은 깃창이다. 문양의 일부가 훼손되어서 알아볼수 없다.", 50, true, "무기"),
                 new ShopItem("붉은 가죽 갑옷", "방어력 +1", "낡은 붉은 갑옷이다. 알 수 없는 문양이 새겨져 있다.", 50, true, "갑옷"),
                 new ShopItem("빛바랜 붉은 샌들", "체력 +1", "빛바랜 붉은 샌들이다. 뭉개진 단추에 어떤 문양이 새겨져있었던거 같다.", 50, true, "신발"),
-                new ShopItem("구멍난 붉은 두건", "체력 +1", "구멍난 붉은 두건이다. 어떤 문양이 희미하게 그려져 있다.", 50, true, "투구"),
+                new ShopItem("구멍난 붉은 투구", "체력 +1", "구멍난 붉은 투구다. 어떤 문양이 희미하게 그려져 있다.", 50, true, "투구"),
              // 필요에 따라 계속 추가 가능
             };
         }
        
         // 합성 결과 아이템 데이터 (특수 아이템 아님)
-        public static string SynthesisResultItemName = "스파르타의 붉은 깃창";// 합성 결과 아이템 이름 변경 가능!!
+        public static string SynthesisResultItemName = "레오니다스 왕의 붉은 깃창";// 합성 결과 아이템 이름 변경 가능!!
         public static Item GetSynthesisResultItem()
         {
             
